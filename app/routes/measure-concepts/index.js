b0d73f4ea295a3ff2b096de665101915f14efb4c
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2021-02-23 12:56:31 +0100

    Remove all things measures
    Measures aren't needed in the redesigned application.

--- app/routes/measure-concepts/index.js
+++
@@ -1,9 +0,0 @@
-import Route from '@ember/routing/route';
-// eslint-disable-next-line ember/no-mixins
-import DataTableRouteMixin from 'ember-data-table/mixins/route';
-
-export default class MeasureConceptsIndexRoute extends Route.extend(
-  DataTableRouteMixin
-) {
-  modelName = 'measure-concept';
-}
