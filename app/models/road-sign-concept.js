3062fd92b915c968d00ec778f2dab3ddf3c0f5e1
anitacaron <anitarcaron@gmail.com>
2021-02-24 11:29:00 +0000

    add reflexive relationships to road-sign-concept model

--- app/models/road-sign-concept.js
+++ app/models/road-sign-concept.js
@@ -1,9 +1,11 @@
 import Model, { attr, hasMany, belongsTo } from '@ember-data/model';
 
-export default class RoadsignconceptModel extends Model {
+export default class RoadSignConceptModel extends Model {
   @attr image;
   @attr meaning;
   @attr roadSignConceptCode;
   @belongsTo('road-sign-concept-status-code') status;
   @hasMany('road-sign-category') categories;
+  @hasMany('road-sign-concept', { inverse: null }) relatedRoadSignConcepts;
+  @hasMany('road-sign-concept', { inverse: null }) subSigns;
 }
