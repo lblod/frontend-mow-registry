d967f604b7fcbd3e2e73bf617030251060bf1a82
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2021-02-24 10:36:41 +0100

    Make sure the app deploys with dutch as the default language
    This makes "nl-be" the default language when doing production builds. Development builds still use "en-us" by default. It's possible to change the language by adding a `?lang=nl/en` query param to the url.

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,10 +1,17 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
+import config from 'mow-registry/config/environment';
 
 export default class ApplicationRoute extends Route {
   @service intl;
 
-  beforeModel() {
-    this.intl.setLocale(['en-us']);
+  model({ lang }) {
+    const supportedLocales = config.supportedLocales;
+    let defaultLocale =
+      supportedLocales[lang] !== undefined
+        ? supportedLocales[lang]
+        : config.defaultLocale;
+
+    this.intl.setLocale([defaultLocale]);
   }
 }
