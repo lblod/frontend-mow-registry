ae6ba2bae9a3d32afff1af2a2fc22b650ceb4a11
anitacaron <anitarcaron@gmail.com>
2021-02-25 12:00:32 +0000

    take all sub signs to edit

--- app/routes/road-sign-concepts/subsigns/edit.js
+++ app/routes/road-sign-concepts/subsigns/edit.js
@@ -1,9 +1,21 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+import { hash } from 'rsvp';
 
 export default class RoadSignConceptsSubsignsEditRoute extends Route {
+  @service store;
+
   model(params) {
-    return this.store.findRecord('road-sign-concept', params.id, {
-      include: 'subSigns',
+    return hash({
+      roadSignConcept: this.store.findRecord('road-sign-concept', params.id),
+      allSubSigns: this.store
+        .query('road-sign-category', {
+          filter: { label: 'Onderbord' },
+          include: 'roadSignConcepts',
+        })
+        .then(function (subSigns) {
+          return subSigns.firstObject;
+        }),
     });
   }
 }
