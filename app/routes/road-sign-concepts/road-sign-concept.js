cb7fd5d1573b6ea46bbd88cebda29c942c0ae6b3
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2021-03-09 13:38:53 +0100

    fix a problem with the sub sign filtering
    The filtering was executed before the sub sign relationship data was loaded, which resulted in no sub signs being filtered out.

--- app/routes/road-sign-concepts/road-sign-concept.js
+++ app/routes/road-sign-concepts/road-sign-concept.js
@@ -6,13 +6,9 @@ export default class RoadsignConcept extends Route {
   @service store;
 
   async model(params) {
-    const roadSignConcept = await this.store.findRecord(
-      'road-sign-concept',
-      params.id
-    );
-
-    const allSubSigns = await this.store
-      .query('road-sign-concept', {
+    let model = await hash({
+      roadSignConcept: this.store.findRecord('road-sign-concept', params.id),
+      allSubSigns: this.store.query('road-sign-concept', {
         filter: {
           categories: {
             label: 'Onderbord',
@@ -21,21 +17,21 @@ export default class RoadsignConcept extends Route {
         page: {
           size: 10000,
         },
-      })
-      .then((subSigns) => {
-        return subSigns.filter(
-          (subSign) =>
-            subSign.id !== roadSignConcept.id &&
-            !roadSignConcept.subSigns.includes(subSign)
-        );
-      });
-    return hash({
-      roadSignConcept,
-      allSubSigns,
+      }),
       categories: this.store.findAll('road-sign-category').then((category) => {
         return category.filter(({ label }) => label !== 'Onderbord');
       }),
     });
+
+    let relatedSubSigns = await model.roadSignConcept.subSigns;
+    model.allSubSigns = model.allSubSigns.filter((subSign) => {
+      return (
+        subSign.id !== model.roadSignConcept.id &&
+        !relatedSubSigns.includes(subSign)
+      );
+    });
+
+    return model;
   }
 
   resetController(controller) {
