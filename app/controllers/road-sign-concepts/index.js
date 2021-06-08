aab939553ffaa3e6a379d7baa16618a41b358502
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2021-06-08 13:07:14 +0200

    Add a category filter

--- app/controllers/road-sign-concepts/index.js
+++ app/controllers/road-sign-concepts/index.js
@@ -1,19 +1,43 @@
 import Controller from '@ember/controller';
+import { action } from '@ember/object';
 import { restartableTask, timeout } from 'ember-concurrency';
 import { tracked } from '@glimmer/tracking';
 
 export default class RoadsignConceptsIndexController extends Controller {
+  queryParams = ['page', 'size', 'search', 'sort', 'category'];
+
   @tracked page = 0;
-  @tracked size = 10;
-  @tracked filter = '';
+  @tracked size = 30;
+  @tracked search = '';
+  @tracked category = null;
   @tracked sort = 'road-sign-concept-code';
 
   @restartableTask
   *updateSearchFilterTask(event) {
     yield timeout(300);
 
-    this.filter = event.target.value;
+    this.search = event.target.value;
     this.resetPagination();
+  }
+
+  get selectedCategory() {
+    if (!this.category) {
+      return null;
+    }
+
+    return this.model.categories.find((category) => {
+      return category.id === this.category;
+    });
+  }
+
+  @action
+  updateCategoryFilter(selectedCategory) {
+    if (selectedCategory) {
+      this.category = selectedCategory.id;
+      this.resetPagination();
+    } else {
+      this.category = null;
+    }
   }
 
   resetPagination() {
