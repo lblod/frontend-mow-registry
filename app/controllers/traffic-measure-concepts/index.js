50436434d6914cc71b88f164791110b4e12e93c4
Sergey Andreev <sergey.andreev@redpencil.io>
2021-10-05 09:03:10 +0200

    cleaned up code a bit

---
+++ app/controllers/traffic-measure-concepts/index.js
@@ -0,0 +1,26 @@
+import Controller from '@ember/controller';
+import { action } from '@ember/object';
+import { restartableTask, timeout } from 'ember-concurrency';
+import { tracked } from '@glimmer/tracking';
+
+export default class TrafficMeasureConceptsIndexController extends Controller {
+  queryParams = ['page', 'size', 'code', 'template', 'sort'];
+
+  @tracked page = 0;
+  @tracked size = 30;
+  @tracked code = '';
+  @tracked template = '';
+  @tracked sort = 'label';
+
+  @restartableTask
+  *updateSearchFilterTask(queryParamProperty, event) {
+    yield timeout(300);
+
+    this[queryParamProperty] = event.target.value.trim();
+    this.resetPagination();
+  }
+
+  resetPagination() {
+    this.page = 0;
+  }
+}
--- app/routes/traffic-measure-concepts/index.js
+++ app/routes/traffic-measure-concepts/index.js
@@ -3,7 +3,28 @@ import { inject as service } from '@ember/service';
 
 export default class TrafficMeasureConceptsIndexRoute extends Route {
   @service store;
-  async model() {
-    return await this.store.findAll('road-measure');
+  
+  queryParams = {
+    code: { refreshModel: true },
+    template: { refreshModel: true },
+    page: { refreshModel: true },
+    size: { refreshModel: true },
+    sort: { refreshModel: true }
+  };
+
+  async model(params) {
+    let query = {
+      sort: params.sort,
+      page: {
+        number: params.page,
+        size: params.size,
+      },
+    };
+
+    if (params.code) {
+      query['filter[label]'] = params.code;
+    }
+    
+    return await this.store.query('road-measure', query);
   }
 }
