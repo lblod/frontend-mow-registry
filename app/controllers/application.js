5dced3c16d241cd02adabb205ceed44cf82301f1
Niels Vandekeybus <progster@gmail.com>
2021-10-15 17:11:27 +0200

    add logout button

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,6 +1,15 @@
 import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
 
 export default class ApplicationController extends Controller {
+  @service session;
+
+  @action
+  logout() {
+    this.session.invalidate();
+  }
+
   queryParams = ['lang'];
 
   lang = '';
