be833596794b06325e5c939011b1557c4636de3c
Sergey Andreev <sergey.andreev@redpencil.io>
2021-11-02 12:12:01 +0100

    fixed bug where the change to the type of a newly created variable wasn't displayed

--- app/components/traffic-measure/index.js
+++ app/components/traffic-measure/index.js
@@ -105,11 +105,11 @@ export default class TrafficMeasureIndexComponent extends Component {
     //add new variable mappings
     filteredRegexResult.forEach((reg) => {
       if (!this.mappings.find((mapping) => mapping.variable === reg[1])) {
-        this.mappings.pushObject({
+        this.mappings.pushObject(this.store.createRecord('mapping', {
           variable: reg[1],
           type: 'text',
           expects: this.nodeShape,
-        });
+        }));
       }
     });
 
@@ -346,9 +346,9 @@ export default class TrafficMeasureIndexComponent extends Component {
     this.parseTemplate();
   }
 
-  @task
-  *updateMappingType(mapping, selectedType) {
-    mapping.type = yield selectedType;
+  @action
+  updateMappingType(mapping, selectedType) {
+    mapping.type = selectedType;
     this.parseTemplate();
   }
 }
