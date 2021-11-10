d81ee4df72e7cfa3f31ba6dba044f44cf41a4a1e
Niels Vandekeybus <progster@gmail.com>
2021-11-10 16:17:40 +0100

    reinstate styled logout button

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,9 +1,16 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
 
 export default class ApplicationController extends Controller {
   @service session;
   queryParams = ['lang'];
 
   lang = '';
+
+  @action
+  logout() {
+    this.session.invalidate();
+  }
+
 }
