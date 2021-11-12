d8ce8f355ac16f1803c46ca215efa180733318ed
Niels Vandekeybus <progster@gmail.com>
2021-11-12 10:59:31 +0100

    refactor road-marking new and edit

---
+++ app/components/road-marking-form.js
@@ -0,0 +1,55 @@
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
+import { dropTask } from 'ember-concurrency';
+import RoadMarkingConceptValidations from 'mow-registry/validations/road-marking-concept';
+
+export default class RoadMarkingFormComponent extends Component {
+  @service router;
+  @service fileService;
+
+  RoadMarkingConceptValidations = RoadMarkingConceptValidations;
+  file;
+
+  get isSaving() {
+    return this.editRoadMarkingConceptTask.isRunning;
+  }
+
+  @action
+  setImageUrl(roadMarkingConcept, event) {
+    roadMarkingConcept.image = event.target.value;
+    this.file = null;
+  }
+
+  @action
+  setImageUpload(roadMarkingConcept, event) {
+    this.file = event.target.files[0];
+    roadMarkingConcept.image = this.file.name;
+  }
+
+  @action
+  setRoadMarkingConceptValue(roadMarkingConcept, attributeName, event) {
+    roadMarkingConcept[attributeName] = event.target.value;
+  }
+
+  @dropTask
+  *editRoadMarkingConceptTask(roadMarkingConcept, event) {
+    event.preventDefault();
+
+    if (this.file) {
+      let fileResponse = yield this.fileService.upload(this.file);
+      roadMarkingConcept.image = fileResponse.downloadLink;
+    }
+
+    yield roadMarkingConcept.validate();
+
+    if (roadMarkingConcept.isValid) {
+      yield roadMarkingConcept.save();
+
+      this.router.transitionTo(
+        'road-marking-concepts.road-marking-concept',
+        roadMarkingConcept.id
+      );
+    }
+  }
+}
