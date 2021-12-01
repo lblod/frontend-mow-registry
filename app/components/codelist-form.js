cfd433b494c20b8d834542194e4fe166a1c2e598
Sergey Andreev <sergey.andreev@redpencil.io>
2021-12-01 15:01:14 +0100

    Bugfix/codelists (#109)
    * added ids to codelists view, removed form from codelist edit view
    
    * fixed an issue where instruction form was submitted when pressing enter
    
    * fixed bug where canceling editing wouldnt clear memory
    
    * fixed bug where deleting and canceling would still presist changes
    
    * fixed linting
    
    * refactored a bit
    
    * fixed bug where newly created codelist was empty
    
    * fixed linting
    
    * fixed linting
    
    * small improvements
    
    Co-authored-by: Niels Vandekeybus <progster@gmail.com>

--- app/components/codelist-form.js
+++ app/components/codelist-form.js
@@ -4,20 +4,20 @@ import { inject as service } from '@ember/service';
 import { dropTask } from 'ember-concurrency';
 import CodelistValidations from 'mow-registry/validations/codelist';
 import { tracked } from '@glimmer/tracking';
-import { A } from '@ember/array';
 
 export default class CodelistFormComponent extends Component {
   @service router;
   @service store;
 
   @tracked newValue = '';
-  @tracked toSave = A();
+  @tracked toDelete = [];
+  @tracked options;
 
   CodelistValidations = CodelistValidations;
 
   constructor() {
     super(...arguments);
-    this.options = this.args.codelistOptions ? this.args.codelistOptions : [];
+    this.options = this.args.codelist.codeListOptions;
   }
 
   get isSaving() {
@@ -35,7 +35,8 @@ export default class CodelistFormComponent extends Component {
   }
 
   @action
-  addNewValue() {
+  addNewValue(event) {
+    event.preventDefault();
     const codeListOption = this.store.createRecord('code-list-option');
     codeListOption.label = this.newValue;
     this.options.pushObject(codeListOption);
@@ -45,8 +46,7 @@ export default class CodelistFormComponent extends Component {
   @action
   removeOption(option) {
     this.options.removeObject(option);
-    this.toSave.pushObject(option);
-    option.deleteRecord();
+    this.toDelete.pushObject(option);
   }
 
   @dropTask
@@ -56,10 +56,38 @@ export default class CodelistFormComponent extends Component {
     yield codelist.validate();
 
     if (codelist.isValid) {
-      yield Promise.all(this.toSave.map((option) => option.save()));
+      yield Promise.all(this.toDelete.map((option) => option.destroyRecord()));
       yield codelist.save();
-      yield codelist.codeListOptions.save();
+      yield Promise.all(this.options.map((option) => option.save()));
       this.router.transitionTo('codelists-management.codelist', codelist.id);
+    }
+  }
+
+  @action
+  cancelEditingTask() {
+    if (this.args.codelist.isNew) {
+      this.router.transitionTo('codelists-management');
+    } else {
+      for (let i = 0; i < this.options.length; i++) {
+        const option = this.options.objectAt(i);
+        if (option.isNew) {
+          option.rollbackAttributes();
+          i--;
+        }
+      }
+
+      for (let i = 0; i < this.toDelete.length; i++) {
+        const option = this.toDelete.objectAt(i);
+        if (!option.isNew) {
+          option.rollbackAttributes();
+          this.options.pushObject(option);
+        }
+      }
+
+      this.router.transitionTo(
+        'codelists-management.codelist',
+        this.args.codelist.id
+      );
     }
   }
 }
