36141654f519fac8bb7d5a5891f45bfe7fa0b015
Sergey Andreev <sergey.andreev@redpencil.io>
2021-12-13 07:45:07 +0100

    refactored everything for easier merge

--- app/models/traffic-measure-concept.js
+++ app/models/traffic-measure-concept.js
@@ -1,6 +1,7 @@
-import { attr } from '@ember-data/model';
+import { attr, belongsTo } from '@ember-data/model';
 import ConceptModel from './concept';
 
 export default class TrafficMeasureConceptModel extends ConceptModel {
   @attr label;
+  @belongsTo('skos-concept', { inverse: null }) zonality;
 }
---
+++ app/utils/constants.js
@@ -0,0 +1,5 @@
+//zonality ids
+export const ZON_NON_ZONAL_ID = 'b651931b-923c-477c-8da9-fc7dd841fdcc';
+export const ZON_ZONAL_ID = 'c81c6b96-736a-48cf-b003-6f5cc3dbc55d';
+export const ZON_POTENTIALLY_ZONAL_ID = '8f9367b2-c717-4be7-8833-4c75bbb4ae1f';
+export const ZON_CONCEPT_SCHEME_ID = '37246d14-d8ad-478c-95b1-2fd57e688b22';
