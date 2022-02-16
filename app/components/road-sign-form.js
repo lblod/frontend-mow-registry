2d3469b1f90134b2aada1cae0cd8f418675fde12
Sergey Andreev <sergey.andreev@redpencil.io>
2022-02-16 09:08:29 +0100

    absolute image paths, please read inside (#125)
    * initial commit
    
    * removed debugger statement, fixed linting

--- app/components/road-sign-form.js
+++ app/components/road-sign-form.js
@@ -3,6 +3,7 @@ import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 import { dropTask } from 'ember-concurrency';
 import RoadSignConceptValidations from 'mow-registry/validations/road-sign-concept';
+import ENV from 'mow-registry/config/environment';
 
 export default class RoadSignFormComponent extends Component {
   @service router;
@@ -43,7 +44,11 @@ export default class RoadSignFormComponent extends Component {
 
     if (this.file) {
       let fileResponse = yield this.fileService.upload(this.file);
-      roadSignConcept.image = fileResponse.downloadLink;
+      if (ENV.baseUrl != '{{BASE_URL}}') {
+        roadSignConcept.image = ENV.baseUrl + fileResponse.downloadLink;
+      } else {
+        roadSignConcept.image = 'http://localhost' + fileResponse.downloadLink;
+      }
     }
 
     yield roadSignConcept.validate();
