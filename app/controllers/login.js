90a5e26137024c3f9f88d2aa94a9ec56513cfc6b
Elena Poelman <elena.poelman@redpencil.io>
2023-02-02 15:03:40 +0100

    update ember-acm-idm login to 2.0.0-beta.1

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -1,7 +1,10 @@
 import Controller from '@ember/controller';
 import ENV from 'mow-registry/config/environment';
-
+import config from 'mow-registry/config/environment';
+import buildUrlFromConfig from '@lblod/ember-acmidm-login/utils/build-url-from-config';
+const providerConfig = config.torii.providers['acmidm-oauth2'];
 export default class LoginController extends Controller {
+  loginUrl = buildUrlFromConfig(providerConfig);
   get showSimpleLogin() {
     const simpleLogin = ENV.featureFlags.simpleLogin;
     if (typeof simpleLogin == 'boolean') {
