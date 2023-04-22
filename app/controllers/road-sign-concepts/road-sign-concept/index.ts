22e175ef5d873f6f660a9d7a0dfbe31cef41078f
Ruben <ruben.de.facq@redpencil.io>
2023-04-22 22:26:50 +0200

    remove refactored away fetches and controller actions for road-sign

--- app/controllers/road-sign-concepts/road-sign-concept/index.ts
+++ app/controllers/road-sign-concepts/road-sign-concept/index.ts
@@ -1,27 +1,11 @@
 import Controller from '@ember/controller';
 import { ModelFrom } from 'mow-registry/utils/type-utils';
 import RoadSignConceptsRoadSignConceptIndexRoute from 'mow-registry/routes/road-sign-concepts/road-sign-concept/index';
-import { task } from 'ember-concurrency';
 
 export default class RoadSignConceptsRoadsignConceptIndexController extends Controller {
   declare model: ModelFrom<RoadSignConceptsRoadSignConceptIndexRoute>;
 
-  show = {};
-
   get isSubSign() {
     return this.model.isSubSign;
   }
-
-  get concept() {
-    return this.model.mainConcept;
-  }
-
-  removeTemplate = task(async (template) => {
-    let templates = await this.concept.templates;
-
-    templates.removeObject(template);
-
-    await template.destroyRecord();
-    await this.concept.save();
-  });
 }
