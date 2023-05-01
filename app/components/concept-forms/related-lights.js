59c107d1c0ccaf043ba10387fe79c79b17ecf7af
abeforgit <arnebertrand@gmail.com>
2023-05-01 16:03:01 +0200

    refactor(concept-pages): remove OOP wiring in concept forms

--- app/components/concept-forms/related-lights.js
+++ app/components/concept-forms/related-lights.js
@@ -1,8 +1,74 @@
 import { restartableTask } from 'ember-concurrency';
 import { trackedTask } from 'ember-resources/util/ember-concurrency';
-import ConceptFormsBaseComponent from './concept-forms-base';
+import { inject as service } from "@ember/service";
+import { tracked } from "@glimmer/tracking";
+import { action } from "@ember/object";
+import Component from "@glimmer/component";
 
-export default class ConceptFormsRelatedLightsComponent extends ConceptFormsBaseComponent {
+export default class ConceptFormsRelatedLightsComponent extends Component {
+
+  @service store;
+  @tracked
+  page = 0;
+  @tracked
+  sort = '';
+  pageSize = 30;
+
+  get model() {
+    return this.args.model;
+  }
+
+  get relatedIds() {
+    return new Set([...this.relatedConcepts.map((concept) => concept.id)]);
+  }
+
+  get relations() {
+    if (this.model.related) {
+      return [this.model.related];
+    } else {
+      return [this.model.relatedFrom, this.model.relatedTo];
+    }
+  }
+
+  get relatedConcepts() {
+    let rel = this.relations;
+    if (rel.length === 1) {
+      return rel[0];
+    } else {
+      return [...rel[0], ...rel[1]];
+    }
+  }
+
+  @action
+  onPageChange(newPage) {
+    this.page = newPage;
+  }
+
+  @action
+  onSortChange(newSort) {
+    this.sort = newSort;
+  }
+
+  @action
+  async addRelated(concept) {
+    for (const rel of this.relations) {
+      rel.pushObject(concept);
+    }
+    await this.model.mainConcept.save();
+  }
+
+  @action
+  async removeRelated(concept) {
+    for (const rel of this.relations) {
+      rel.removeObject(concept);
+    }
+
+    await this.model.mainConcept.save();
+  }
+
+  isRelated = (concept) => {
+    return this.relatedIds.has(concept.id);
+  };
   loadPotentialConcepts = restartableTask(async ({ page, sort, pageSize }) => {
     return this.store.query('traffic-light-concept', {
       sort,
