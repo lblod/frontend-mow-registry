466221f66bb2048e0480932160cf69a0c5acba23
Elena Poelman <elena.poelman@redpencil.io>
2023-06-09 11:36:23 +0200

    use js for application route and session service due to conflicts with ember-simple-auth

--- app/services/session.js
+++ app/services/session.js
@@ -1,11 +1,9 @@
-//@ts-expect-error types of ember-simple-auth are not yet published
 import BaseSessionService from 'ember-simple-auth/services/session';
 import ENV from 'mow-registry/config/environment';
 
 export default class SessionService extends BaseSessionService {
   handleInvalidation() {
     const logoutUrl = ENV['torii']['providers']['acmidm-oauth2']['logoutUrl'];
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
     super.handleInvalidation(logoutUrl);
   }
 }
