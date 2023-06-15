f89b1fa15cffc4dcafcee66aeea6a7c3457d8c35
Elena Poelman <elena.poelman@redpencil.io>
2023-06-15 11:12:09 +0200

    use one-way binding in mu-login component

--- app/components/mu-login.js
+++ app/components/mu-login.js
@@ -1,3 +1,15 @@
+import { tracked } from '@glimmer/tracking';
 import MuLoginComponent from 'ember-mu-login/components/mu-login';
 
-export default class MyMuLoginComponent extends MuLoginComponent {}
+export default class MyMuLoginComponent extends MuLoginComponent {
+  @tracked nickname;
+  @tracked password;
+
+  setNickname(event) {
+    this.nickname = event.target.value;
+  }
+
+  setPassword(event){
+    this.password = event.target.value;
+  }
+}
