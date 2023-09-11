411bb4ae29ac979261028b57cbf924e6f19171d9
Elena Poelman <elena.poelman@redpencil.io>
2023-09-11 10:43:07 +0200

    addition of a zonality-helper

---
+++ app/helpers/zonality-label.ts
@@ -0,0 +1,16 @@
+import { inject as service } from '@ember/service';
+import Helper from '@ember/component/helper';
+import IntlService from 'ember-intl/services/intl';
+import SkosConcept from 'mow-registry/models/skos-concept';
+
+export default class ZonalityLabel extends Helper {
+  @service declare intl: IntlService;
+  compute([zonality]: [SkosConcept | undefined]) {
+    const label = zonality?.label;
+    if (label) {
+      return this.intl.t('utility.' + label);
+    } else {
+      return this.intl.t('utility.nonZonal');
+    }
+  }
+}
