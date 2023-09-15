089643f1071a216301482a2663090a0b5d6db768
Elena Poelman <elena.poelman@redpencil.io>
2023-09-15 14:09:07 +0200

    replace eslint-plugin-node by eslint-plugin-n

--- .eslintrc.js
+++ .eslintrc.js
@@ -52,14 +52,7 @@ module.exports = {
         browser: true,
         node: true,
       },
-      plugins: ['node'],
-      extends: ['plugin:node/recommended'],
-      // eslint-disable-next-line @typescript-eslint/no-var-requires
-      rules: {
-        // this can be removed once the following is fixed
-        // https://github.com/mysticatea/eslint-plugin-node/issues/77
-        'node/no-unpublished-require': 'off',
-      },
+      extends: ['plugin:n/recommended'],
     },
     // typescript
     {
