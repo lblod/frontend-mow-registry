787e3448c7711642c5796215a5d6b5059f8efd52
Andreo Van Onckelen <andreo141mlg@gmail.com>
2024-05-07 11:33:44 +0200

    wip: adding document model

--- app/models/document.ts
+++ app/models/document.ts
@@ -1,7 +1,9 @@
-import Model, { AsyncBelongsTo, attr, belongsTo } from '@ember-data/model';
+import Model from '@ember-data/model';
 
 declare module 'ember-data/types/registries/model' {
   export default interface ModelRegistry {
     document: DocumentModel;
   }
 }
+
+export default class DocumentModel extends Model {}
