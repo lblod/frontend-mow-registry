3663a4e2c033e36eee82a910af79489e0fcdc865
Elena Poelman <elena.poelman@redpencil.io>
2024-06-13 13:27:35 +0200

    ensure default zonality of non-zonal for road-signs and traffic-measures

--- app/routes/traffic-measure-concepts/new.ts
+++ app/routes/traffic-measure-concepts/new.ts
@@ -1,14 +1,22 @@
 import Store from '@ember-data/store';
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
+import { ZON_NON_ZONAL_ID } from 'mow-registry/utils/constants';
 
 export default class TrafficMeasureConceptsNewRoute extends Route {
   @service declare store: Store;
 
-  model() {
+  async model() {
+    const nonZonalConcept = await this.store.findRecord(
+      'skos-concept',
+      ZON_NON_ZONAL_ID,
+    );
     const template = this.store.createRecord('template');
     const trafficMeasureConcept = this.store.createRecord(
       'traffic-measure-concept',
+      {
+        zonality: nonZonalConcept,
+      },
     );
 
     template.value = '';
