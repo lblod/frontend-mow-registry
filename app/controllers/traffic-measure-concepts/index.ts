e7e524b51e4ad6c004b172d355185c5223387561
Andreo Van Onckelen <andreo141mlg@gmail.com>
2024-06-14 13:39:52 +0200

    update queryparams + renaming

--- app/controllers/traffic-measure-concepts/index.ts
+++ app/controllers/traffic-measure-concepts/index.ts
@@ -4,16 +4,16 @@ import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 
 export default class TrafficMeasureConceptsIndexController extends Controller {
-  queryParams = ['page', 'size', 'code', 'template', 'sort'];
+  queryParams = ['page', 'size', 'label', 'template', 'sort'];
 
   @tracked page = 0;
   @tracked size = 30;
-  @tracked code = '';
+  @tracked label = '';
   @tracked template = null;
   @tracked sort = 'label';
 
   updateSearchFilterTask = restartableTask(
-    async (queryParamProperty: 'code', event: InputEvent) => {
+    async (queryParamProperty: 'label', event: InputEvent) => {
       await timeout(300);
 
       this[queryParamProperty] = (event.target as HTMLInputElement).value;
