cc64f2822663d44e7fc0c09165c5e31026a88928
Andreo Van Onckelen <andreo141mlg@gmail.com>
2024-06-14 13:37:37 +0200

    WIP: road sign concepts

--- app/validations/road-sign-concept.ts
+++ app/validations/road-sign-concept.ts
@@ -4,10 +4,10 @@ import {
 } from 'ember-changeset-validations/validators';
 
 export default {
-  image: validatePresence({ presence: true, ignoreBlank: true }),
+  // image: validatePresence({ presence: true, ignoreBlank: true }),
   meaning: validatePresence({ presence: true, ignoreBlank: true }),
-  code: validatePresence({ presence: true, ignoreBlank: true }),
+  label: validatePresence({ presence: true, ignoreBlank: true }),
   classifications: validateLength({ min: 1 }),
   shape: validatePresence({ presence: true, ignoreBlank: true }),
-  dimensions: validatePresence({ presence: true, ignoreBlank: true }),
+  // dimensions: validatePresence({ presence: true, ignoreBlank: true }),
 };
