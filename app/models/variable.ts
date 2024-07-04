c7b1f34b2ecec3aabb9be4db2827d408dfe52c4b
Andreo Van Onckelen <andreo141mlg@gmail.com>
2024-07-04 11:27:00 +0200

    create variable model

---
+++ app/models/variable.ts
@@ -0,0 +1,27 @@
+import Model, { AsyncBelongsTo, belongsTo, attr } from '@ember-data/model';
+import type CodeListModel from './code-list';
+import type TemplateModel from './template';
+import type ResourceModel from './resource';
+
+declare module 'ember-data/types/registries/model' {
+  export default interface ModelRegistry {
+    variable: VariableModel;
+  }
+}
+
+export default class VariableModel extends Model {
+  @attr declare label?: string;
+  @attr declare type?: string;
+
+  @belongsTo('resource', { inverse: null, async: true })
+  declare defaultValue: AsyncBelongsTo<ResourceModel>;
+
+  @belongsTo('code-list', { inverse: null, async: true })
+  declare codeList: AsyncBelongsTo<CodeListModel>;
+
+  @belongsTo('template', { inverse: null, async: true })
+  declare template: AsyncBelongsTo<TemplateModel>;
+
+  @belongsTo('resource', { inverse: null, async: true })
+  declare value: AsyncBelongsTo<ResourceModel>;
+}
