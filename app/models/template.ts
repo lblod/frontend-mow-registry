fda7df21b8f7f59e5cad0beb34cb90e348d1fa47
Andreo Van Onckelen <andreo141mlg@gmail.com>
2024-07-08 12:21:19 +0200

    extend to DocumentModel on TemplateModel

--- app/models/template.ts
+++ app/models/template.ts
@@ -7,13 +7,14 @@ import Model, {
 } from '@ember-data/model';
 import type MappingModel from 'mow-registry/models/mapping';
 import TrafficMeasureConceptModel from './traffic-measure-concept';
+import DocumentModel from './document';
 
 declare module 'ember-data/types/registries/model' {
   export default interface ModelRegistry {
     template: TemplateModel;
   }
 }
-export default class TemplateModel extends Model {
+export default class TemplateModel extends DocumentModel {
   @attr('string') declare value?: string;
   @attr('string') declare annotated?: string;
 
