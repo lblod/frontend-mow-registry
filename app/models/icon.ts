9f48e4cf168ef06b80c81ec9d7d18c7e4fa84423
Laurent Deketelaere <laurent.deketelaere@redpencil.io>
2024-07-24 14:19:15 +0200

    refactor: Update image source in icon catalog component

--- app/models/icon.ts
+++ app/models/icon.ts
@@ -1,6 +1,6 @@
 import { AsyncBelongsTo, belongsTo } from '@ember-data/model';
 import SkosConcept from 'mow-registry/models/skos-concept';
-import FileModel from 'mow-registry/models/file';
+import ImageModel from 'mow-registry/models/image';
 
 declare module 'ember-data/types/registries/model' {
   export default interface ModelRegistry {
@@ -8,8 +8,9 @@ declare module 'ember-data/types/registries/model' {
   }
 }
 export default class Icon extends SkosConcept {
-  @belongsTo('file', {
+  @belongsTo('image', {
     async: true,
+    inverse: null,
   })
-  declare image: AsyncBelongsTo<FileModel>;
+  declare image: AsyncBelongsTo<ImageModel>;
 }
