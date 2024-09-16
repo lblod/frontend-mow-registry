f8d3ba65242b02ec364c55382996c43b66874c39
Andreo Van Onckelen <andreo141mlg@gmail.com>
2024-09-16 17:20:11 +0200

    remove unused relationships

--- app/models/resource.ts
+++ app/models/resource.ts
@@ -1,15 +1,10 @@
-import { AsyncHasMany, hasMany } from '@ember-data/model';
 import Joi from 'joi';
 import AbstractValidationModel from './abstract-validation-model';
 import { validateHasManyOptional } from 'mow-registry/validators/schema';
-import type Concept from 'mow-registry/models/concept';
 import type { Type } from '@warp-drive/core-types/symbols';
 
 export default class Resource extends AbstractValidationModel {
   declare [Type]: 'resource';
-
-  @hasMany<Concept>('concept', { inverse: null, async: true })
-  declare used: AsyncHasMany<Concept>;
 
   get validationSchema() {
     return Joi.object({
--- app/models/shape.ts
+++ app/models/shape.ts
@@ -1,6 +1,5 @@
 import Model, { AsyncBelongsTo, belongsTo } from '@ember-data/model';
 import type Resource from 'mow-registry/models/resource';
-import type Concept from 'mow-registry/models/concept';
 import type { Type } from '@warp-drive/core-types/symbols';
 
 export default class Shape extends Model {
@@ -18,10 +17,4 @@ export default class Shape extends Model {
     polymorphic: true,
   })
   declare targetNode: AsyncBelongsTo<Resource>;
-  @belongsTo<Concept>('concept', {
-    inverse: null,
-    async: true,
-    polymorphic: true,
-  })
-  declare targetHasConcept: AsyncBelongsTo<Concept>;
 }
