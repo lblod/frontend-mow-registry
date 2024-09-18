1437edb6a63a9eb7fba82e09a893481ebecf0a07
Andreo Van Onckelen <andreo141mlg@gmail.com>
2024-09-18 10:27:57 +0200

    modify content check script to use correct values

--- app/components/content-check.ts
+++ app/components/content-check.ts
@@ -1,13 +1,13 @@
 import Component from '@glimmer/component';
 import { task } from 'ember-concurrency';
-import Concept from 'mow-registry/models/concept';
+import TrafficSignConcept from 'mow-registry/models/traffic-sign-concept';
 
 type Args = {
-  concept: Concept;
+  trafficSignConcept: TrafficSignConcept;
 };
 export default class ContentCheckComponent extends Component<Args> {
   update = task(async (value: boolean) => {
-    this.args.concept.valid = value;
-    await this.args.concept.save();
+    this.args.trafficSignConcept.valid = value;
+    await this.args.trafficSignConcept.save();
   });
 }
