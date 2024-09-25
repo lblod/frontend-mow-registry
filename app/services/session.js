584d1a54dd5cb7a8ad2d52ad83eea3f5d19a4c78
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2024-09-25 10:14:33 +0200

    Fix the logout url setup
    We now only use the logoutUrl value if it is actually configured. Environments that don't use ACM/IDM don't need a custom logout url.

--- app/services/session.js
+++ app/services/session.js
@@ -2,8 +2,13 @@ import BaseSessionService from 'ember-simple-auth/services/session';
 import ENV from 'mow-registry/config/environment';
 
 export default class SessionService extends BaseSessionService {
-  handleInvalidation() {
+  handleInvalidation(routeAfterInvalidation) {
     const logoutUrl = ENV['torii']['providers']['acmidm-oauth2']['logoutUrl'];
-    super.handleInvalidation(logoutUrl);
+
+    if (!logoutUrl.startsWith('{{')) {
+      super.handleInvalidation(logoutUrl);
+    } else {
+      super.handleInvalidation(routeAfterInvalidation);
+    }
   }
 }
