af2c4a0d3034a97358e8786d41b48152710cdf67
abeforgit <arnebertrand@gmail.com>
2025-01-14 20:38:47 +0100

    fix: remove unused logic from traffic lights controller

--- app/controllers/traffic-light-concepts/index.ts
+++ app/controllers/traffic-light-concepts/index.ts
@@ -5,14 +5,13 @@ import { tracked } from '@glimmer/tracking';
 import type { ModelFrom } from 'mow-registry/utils/type-utils';
 import TrafficlightConceptsIndexRoute from 'mow-registry/routes/traffic-light-concepts/index';
 
-export default class RoadsignConceptsIndexController extends Controller {
-  queryParams = ['page', 'size', 'label', 'meaning', 'sort', 'classification'];
+export default class TrafficlightConceptsIndexController extends Controller {
+  queryParams = ['page', 'size', 'label', 'meaning', 'sort'];
   declare model: ModelFrom<TrafficlightConceptsIndexRoute>;
   @tracked page = 0;
   @tracked size = 30;
   @tracked label = '';
   @tracked meaning = '';
-  @tracked classification: string | null = null;
   @tracked sort = ':no-case:label';
 
   get hasActiveFilter() {
@@ -27,30 +26,6 @@ export default class RoadsignConceptsIndexController extends Controller {
       this.resetPagination();
     },
   );
-
-  get selectedClassification() {
-    if (!this.classification) {
-      return null;
-    }
-    //@ts-expect-error classifications is not defined on model
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-    return this.model.classifications.find((classification) => {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-      return classification.id === this.classification;
-    });
-  }
-
-  @action
-  //@ts-expect-error this.classification is not defined
-  updateCategoryFilter(selectedClassification) {
-    if (selectedClassification) {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
-      this.classification = selectedClassification.id;
-      this.resetPagination();
-    } else {
-      this.classification = null;
-    }
-  }
 
   @action onPageChange(newPage: number) {
     this.page = newPage;
