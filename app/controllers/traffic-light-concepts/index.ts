f91090b4a0012e5516dace8d9284293799fb3c34
abeforgit <arnebertrand@gmail.com>
2025-01-14 20:42:16 +0100

    feat: add validation filter for traffic lights

--- app/controllers/traffic-light-concepts/index.ts
+++ app/controllers/traffic-light-concepts/index.ts
@@ -4,18 +4,32 @@ import { restartableTask, timeout } from 'ember-concurrency';
 import { tracked } from '@glimmer/tracking';
 import type { ModelFrom } from 'mow-registry/utils/type-utils';
 import TrafficlightConceptsIndexRoute from 'mow-registry/routes/traffic-light-concepts/index';
+import { service } from '@ember/service';
+import type IntlService from 'ember-intl/services/intl';
 
 export default class TrafficlightConceptsIndexController extends Controller {
-  queryParams = ['page', 'size', 'label', 'meaning', 'sort'];
+  queryParams = ['page', 'size', 'label', 'meaning', 'sort', 'validation'];
   declare model: ModelFrom<TrafficlightConceptsIndexRoute>;
+
+  @service
+  declare intl: IntlService;
+
   @tracked page = 0;
   @tracked size = 30;
   @tracked label = '';
   @tracked meaning = '';
   @tracked sort = ':no-case:label';
+  @tracked validation?: string | null;
+
+  get validationStatusOptions() {
+    return [
+      { value: 'true', label: this.intl.t('validation-status.valid') },
+      { value: 'false', label: this.intl.t('validation-status.draft') },
+    ];
+  }
 
   get hasActiveFilter() {
-    return Boolean(this.label || this.meaning);
+    return Boolean(this.label || this.meaning || this.validation);
   }
 
   updateSearchFilterTask = restartableTask(
@@ -26,6 +40,23 @@ export default class TrafficlightConceptsIndexController extends Controller {
       this.resetPagination();
     },
   );
+
+  get selectedValidationStatus() {
+    return this.validationStatusOptions.find(
+      (option) => option.value === this.validation,
+    );
+  }
+  @action
+  updateValidationFilter(
+    selectedValidationStatus: (typeof this.validationStatusOptions)[number],
+  ) {
+    if (selectedValidationStatus) {
+      this.validation = selectedValidationStatus.value;
+      this.resetPagination();
+    } else {
+      this.validation = null;
+    }
+  }
 
   @action onPageChange(newPage: number) {
     this.page = newPage;
@@ -38,6 +69,7 @@ export default class TrafficlightConceptsIndexController extends Controller {
   resetFilters = () => {
     this.label = '';
     this.meaning = '';
+    this.validation = null;
     this.resetPagination();
   };
 
