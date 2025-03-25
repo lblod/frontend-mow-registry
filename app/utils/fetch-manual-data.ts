649a6ed521716ce93b6928b80253cf12e3ef668a
Oscar <lagartoverde97@gmail.com>
2025-03-25 16:04:51 +0100

    fix bug with filters

--- app/utils/fetch-manual-data.ts
+++ app/utils/fetch-manual-data.ts
@@ -217,17 +217,27 @@ function generateValidityFilter({
       filter.push(`
         BIND(!bound(?startDate) AS ?noStartDate)
         FILTER(?startDate <= ${sparqlEscapeDateTime(startDate)} || ?noStartDate)
-        BIND(!bound(?endDate) AS ?noEndDate)
-        FILTER(?endDate >= ${sparqlEscapeDateTime(startDate)} || ?noEndDate)
+
       `);
+      if (!endDate) {
+        filter.push(`
+          BIND(!bound(?endDate) AS ?noEndDate)
+          FILTER(?endDate >= ${sparqlEscapeDateTime(startDate)} || ?noEndDate)
+        `);
+      }
     }
     if (endDate) {
       filter.push(`
         BIND(!bound(?endDate) AS ?noEndDate)
-        FILTER(?endDate >= ${sparqlEscapeDateTime(endDate)} || ?noEndDate
-        BIND(!bound(?startDate) AS ?noStartDate)
-        FILTER(?startDate <= ${sparqlEscapeDateTime(endDate)} || ?noStartDate)
+        FILTER(?endDate >= ${sparqlEscapeDateTime(endDate)} || ?noEndDate)
+
       `);
+      if (!startDate) {
+        filter.push(`
+          BIND(!bound(?startDate) AS ?noStartDate)
+          FILTER(?startDate <= ${sparqlEscapeDateTime(endDate)} || ?noStartDate)
+        `);
+      }
     }
     return filter.join(' ');
   }
