a7a75ed787269ad95c383426f320aa0ea1d09c4c
Oscar <lagartoverde97@gmail.com>
2025-07-24 10:58:34 +0200

    remove change not relevant for this PR

--- app/models/traffic-measure-concept.ts
+++ app/models/traffic-measure-concept.ts
@@ -48,7 +48,7 @@ export default class TrafficMeasureConcept extends AbstractValidationModel {
       variableSignage: validateBooleanOptional(),
       valid: validateBooleanOptional(),
       zonality: validateBelongsToRequired(),
-      relatedTrafficSignConceptsOrdered: validateHasManyOptional(),
+      relatedTrafficSignConcepts: validateHasManyOptional(),
       template: validateBelongsToOptional(),
       startDate: validateDateOptional(),
       endDate: validateEndDate(),
