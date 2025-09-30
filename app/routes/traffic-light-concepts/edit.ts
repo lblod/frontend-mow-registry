9f986498f937739f82c70124d13d933e8984ec5d
Elena Poelman <elena.poelman@redpencil.io>
2025-09-30 15:44:26 +0200

    linting

--- app/routes/traffic-light-concepts/edit.ts
+++ app/routes/traffic-light-concepts/edit.ts
@@ -17,11 +17,7 @@ export default class TrafficLightConceptsEditRoute extends Route {
       trafficLightConcept: this.store
         .request(
           findRecord<TrafficLightConcept>('traffic-light-concept', params.id, {
-            include: [
-              'image.file',
-              'variables',
-              'zonality.inScheme.concepts',
-            ],
+            include: ['image.file', 'variables', 'zonality.inScheme.concepts'],
           }),
         )
         .then((res) => res.content),
